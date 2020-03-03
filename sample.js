a94a2eff9487dc1a995555723eeafd6880e55890
guru-lab <58967834+guru-lab@users.noreply.github.com>
2020-03-03 11:15:55 +0530

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+<body>
+<h2> Welcome to js </h2>
+<button type ="button" 
+onclick="document.getElementById('demo').innerHTML = Date()";>
+click to display the date </button>
+<p id="demo"></p>
+</body>
+</html>
